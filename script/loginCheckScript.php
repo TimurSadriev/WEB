baf662aaf128b1f6fc9e92a6e50aa450ed299f8c
TimurSadriev <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>
2022-04-08 19:38:32 +0500

    Залил каталов массив если чо отменим потом + поробовал вызвать жаваскрипт

--- script/loginCheckScript.php
+++ script/loginCheckScript.php
@@ -10,12 +10,12 @@
                return void;
            }
        }
-       echo "Неверный логин/пароль";
+       echo "<script> alert('Неверный лоигин/пароль'); </script>";
    }
    function addClient($loginR, $passwordR,$passwordRC,$email){
        if($passwordR==$passwordRC){
            if(strlen($passwordRC)<3){
-               echo "Пароль должен быть хотя бы 4 символа";
+               echo "<script> alert('Пароль должен быть длиннее 3 символов'); </script>";
            }else {
                $newId = getArray("SELECT MAX(id) FROM users")[0]['MAX(id)'] + 1;
                mysqli_query($_SESSION["link"], "INSERT INTO users (id,login,pass,email,status) VALUES ('$newId','$loginR','$passwordR','$email','0')");
@@ -23,7 +23,7 @@
                header("Refresh:0 url=PersonalArea.php");
            }
            }else{
-           echo "Пароли не совпадают";
+           echo "<script> alert('Пароли не совпадают'); </script>";
        }
    }
 ?>
